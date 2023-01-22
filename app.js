9990ca1f099085825954534130ed0f903c7fbda4
frankjay <frankjr212711@gmail.com>
2023-01-22 11:22:45 +0000

    commented fetch API

---
+++ app.js
@@ -0,0 +1,12 @@
+
+let clientID = 'qtJVWuRtFpo';
+let endPoint = `https://unsplash.com/photos/${clientID}`
+
+
+// fetch(endPoint) 
+//     .then((response) => {
+//         console.log(response.json())
+//     })
+
+
+console.log(endPoint);
